a9298ba04976779ea3b61f1d1cc57f81e7f23555
Adrien <adrien.mechin@gmail.com>
2019-06-24 14:43:10 +0200

    quete 14 - les fixtures

--- src/DataFixtures/ArticlesFixtures.php
+++ src/DataFixtures/ArticlesFixtures.php
@@ -3,8 +3,32 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Article;
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
 
-class ArticlesFixtures
+
+class ArticlesFixtures extends Fixture implements DependentFixtureInterface
 {
+    public function load(ObjectManager $manager)
+    {
+        $faker  =  Faker\Factory::create('fr_FR');
+        for($i=0; $i<50; $i++){
+            $article = new Article();
+            $article->setTitle(mb_strtolower($faker->sentence()));
+            $article->setContent(mb_strtolower($faker->paragraph));
+            $article->setCategory($this->getReference('categorie_'. rand(0, 4)));
+            $manager->persist($article);
+        }
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [CategoryFixtures::class];
+    }
 
 }
